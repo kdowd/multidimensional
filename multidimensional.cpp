3555fb9c85a9c1f5c50f8d1246acbc55b548612c
kdowd <kevin.dowd@gmail.com>
2023-05-08 11:05:05 +1200

    Add project files.

---
+++ multidimensional.cpp
@@ -0,0 +1,26 @@
+// multidimensional.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string> 
+#include <array> 
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    vector<string> xxx = {"d","f"};
+    array<int, 6> testy = {4,6,7,8,9};
+
+
+    int testytwo[] = { 24,78,99 };
+
+    for (auto i : xxx) {
+        cout << i << "\n";
+    }
+    
+}
+
